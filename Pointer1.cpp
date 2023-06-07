3959b5ad0ceeeeb95853a9befd1cdd16d0e20e12
PindropHailstorm <119025864+PindropHailstorm@users.noreply.github.com>
2023-06-07 09:33:49 +0530

    Cpp assignments

---
+++ Pointer1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // declare variables
+    int var1 = 10;
+    int var2 = 20;
+    int var3 = 130;
+
+    // print address of var1
+    cout << "Address of var1: "<< &var1 << endl;
+
+    // print address of var2
+    cout << "Address of var2: " << &var2 << endl;
+
+    // print address of var3
+    cout << "Address of var3: " << &var3 << endl;
+}
